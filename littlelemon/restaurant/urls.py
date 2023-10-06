eb72245040c00df587efdf66776ab43d801fdf06
philo <parkerjimmy6@gmail.com>
2023-10-06 10:45:13 +0800

    adding readme.txt

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,7 +4,6 @@ from rest_framework import routers
 app_name = 'api'
 
 urlpatterns = [
-    path('',views.index,name='index'),
     path('menu/', views.MenuItemsView.as_view(),name='menu-list'),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
